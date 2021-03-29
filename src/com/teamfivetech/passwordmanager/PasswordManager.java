e19d54900775bace3e2fd421279217bd8ac3d179
raymond-sde <raymond.aranda.sde@gmail.com>
2021-03-29 09:50:59 -0700

    Pass Prompter in ctor of PasswordManager

--- src/com/teamfivetech/passwordmanager/PasswordManager.java
+++ src/com/teamfivetech/passwordmanager/PasswordManager.java
@@ -1,41 +1,44 @@
 package com.teamfivetech.passwordmanager;
-
 import com.apps.util.Prompter;
-
 import java.util.*;
 
 public class PasswordManager {
     private static final String LINE_SEPARATOR = new String(new char[30]).replace("\0", "-");
     private static final int MENU_SELECTION_MIN = 1;
 
+    private final Prompter prompter;
+
+    public PasswordManager(Prompter prompter) {
+        this.prompter = prompter;
+    }
+
     public void start() {
         System.out.println(LINE_SEPARATOR);
         System.out.println("Welcome to Password Manager");
 
         String selection = null;
-        Prompter prompter = new Prompter(new Scanner(System.in));
         List<String> menuOptions = Arrays.asList("Store Password", "List Passwords", "Quit");
 
         while(!"3".equals(selection)) {
             printMenu("main menu", menuOptions);
-            selection = printNumberPrompt(prompter, 3);
+            selection = printNumberPrompt(3);
 
-            if ("1".equals(selection)) storePassword(prompter);
+            if ("1".equals(selection)) storePassword();
             if ("2".equals(selection)) listPasswords();
             if ("3".equals(selection)) thankUser();
         }
     }
 
-    public void storePassword(Prompter prompter) {
+    public void storePassword() {
         String selection = null;
         List<String> menuOptions = Arrays.asList("Enter New Password", "Generate New Password", "Back to Main Menu", "Quit");
 
         while (true) {
             printMenu("store password", menuOptions);
-            selection = printNumberPrompt(prompter, 4);
+            selection = printNumberPrompt(4);
 
             if ("1".equals(selection)) enterPassword();
-            if ("2".equals(selection)) generatePassword(prompter);
+            if ("2".equals(selection)) generatePassword();
             if ("3".equals(selection)) break;
             if ("4".equals(selection)) {
                 thankUser();
@@ -59,9 +62,9 @@ public class PasswordManager {
     }
 
     // TODO: implement Password Generator / CSV write functionality
-    public void generatePassword(Prompter prompter) {
-        String userPrompt = "Please enter level of security (Easy, Medium, Hard): ";
-        String securityLevelRegex = "(?i)^easy$|^medium$|^hard$";
+    public void generatePassword() {
+        String userPrompt = "Please enter level of security (Low, Medium, Hard): ";
+        String securityLevelRegex = "(?i)^low$|^medium$|^hard$";
         String validationErrorMsg = "That is not a valid security level";
         String securityLevel = prompter.prompt(userPrompt, securityLevelRegex, validationErrorMsg);
         System.out.println("Selected: " + securityLevel);
@@ -81,7 +84,7 @@ public class PasswordManager {
         System.out.println(LINE_SEPARATOR);
     }
 
-    private String printNumberPrompt(Prompter prompter, int max) {
+    private String printNumberPrompt(int max) {
         String userPrompt = "Please enter a number " + MENU_SELECTION_MIN + " - " + max + ": ";
         String rangeRegex = "[" + MENU_SELECTION_MIN + "-" + max + "]";
         String validationErrorMsg = "That is not a valid number";

a81ea9ee9d242cdc8637c4600d82150aa8ca455e
Seth McAllister <sethm.sde@gmail.com>
2021-03-31 15:47:38 -0400

    refactored if statements to switch

--- src/com/teamfivetech/passwordmanager/controller/PasswordManager.java
+++ src/com/teamfivetech/passwordmanager/controller/PasswordManager.java
@@ -61,29 +61,24 @@ public class PasswordManager {
             printMenu("password options", menuOptions);
             selection = promptNumberRange(5);
 
-            if ("1".equals(selection)) {
-                password = getPrompter().prompt("Enter new password: ");
-                break;
+            switch (selection) {
+                case "1":
+                    password = getPrompter().prompt("Enter new password: ");
+                    break;
+                case "2":
+                    password = generatePasswordFromSecurityLevel();
+                    break;
+                case "3":
+                    password = generateCustomPassword();
+                    break;
+                case"4":
+                    break;
+                case "5":
+                    thankUser();
+                    System.exit(0);
             }
-
-            if ("2".equals(selection)) {
-                password = generatePasswordFromSecurityLevel();
-                break;
-            }
-
-            if ("3".equals(selection)) {
-                password = generateCustomPassword();
-                break;
-            }
-
-            if ("4".equals(selection)) break;
-
-            if ("5".equals(selection)) {
-                thankUser();
-                System.exit(0);
-            }
-        }
         return password;
+        }
     }
 
     private void listLogins() {
